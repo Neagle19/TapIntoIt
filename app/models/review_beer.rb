6b048f89218a870d9897ee34308b76812ddcd338
Diego Saverys <diegosaverys@gmail.com>
2019-03-11 13:42:58 +0100

    added has_many relations

--- app/models/review_beer.rb
+++ app/models/review_beer.rb
@@ -1,4 +1,5 @@
 class ReviewBeer < ApplicationRecord
   belongs_to :beer
   belongs_to :user
+  has_many :comment_beers
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :sent_friend_connections, :class_name => 'Friend_connection', :foreign_key => 'requester_id'
   has_many :received_friend_connections, :class_name => 'Friend_connection', :foreign_key => 'receiver_id'
+  has_many :review_breweries
+  has_many :review_beers
+  has_many :comment_beers
+  has_many :comment_breweries
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
