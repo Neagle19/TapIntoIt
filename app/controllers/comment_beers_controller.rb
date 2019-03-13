591e6719d606d5bf693173c0ae72a15e18d03723
RomanVL <romanvanloo@icloud.com>
2019-03-13 17:43:02 +0100

    comment form in wall

--- app/controllers/comment_beers_controller.rb
+++ app/controllers/comment_beers_controller.rb
@@ -1,2 +1,19 @@
 class CommentBeersController < ApplicationController
+  def create
+    comment = CommentBeer.new(beer_params)
+    comment.user = current_user
+    puts "-"*60
+    puts params
+    if comment.save
+      redirect_to posts_path
+    else
+      redirect_to posts_path
+    end
+  end
+
+  private
+
+  def beer_params
+    params.require(:comment_beer).permit(:content, :review_beer_id, :review_beer)
+  end
 end
