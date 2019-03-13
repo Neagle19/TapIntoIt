ee4a86cd5bee62fb1afc5e6573c06814fc116a8c
Thierry <zthi@hotmail.com>
2019-03-13 14:59:33 +0100

    profilepending

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,5 @@
 class ProfilesController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @cars = Car.where(user_id: @user.id)
+    @brewery = Brewery.where(user_id: @user.id)
   end
