b2bb3d08b3053ec37b1796948a78dec4cce46033
Shane <shane@genuine-standards.com>
2019-03-13 12:25:20 +0100

    added basic QR code feature, but no link to it yet

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :breweries, only: [:index, :show]
+  resources :qr_codes, only: [:new, :create]
+  #   get 'new', to: 'qr_codes#new'
+  #   post 'create', to: 'qr_codes#create'
+  # end
+   # resources :qr_codes, only:[:new, :create]
 end
