931739b3e6a15892a16199125c709d0fbcdae232
Thierry <zthi@hotmail.com>
2019-03-14 17:33:04 +0100

    layout profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,13 @@ class ProfilesController < ApplicationController
   def show
     @user = User.find(params[:id])
     @brewery = Brewery.where(user_id: @user.id)
+    # @received_friend_connections = @user.received_friend_connections.count
+    # @sent_friend_connections = @user.sent_friend_connections.count
+    # @friend_connections = @received_friend_connections + @sent_friend_connections
+    @review_beers = @user.review_beers.count
+    @review_breweries = @user.review_breweries.count
+    @review_comments = @user.review_beers.last(5)
     # @posts = User.find_by(user_name: params[:user_name]).posts.order('created_at DESC')
   end
 end
+
