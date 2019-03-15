ed78f005eb32f3a20473fc1eac71c2bd0329cf1c
Thierry <zthi@hotmail.com>
2019-03-15 11:56:52 +0100

    roman shit

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,6 +1,6 @@
 class Batch < ApplicationRecord
   belongs_to :brewery
-  belongs_to :beer
+  # belongs_to :beer
   validates :bottled_date, presence: true
   validates :brewery, presence: true
 end
