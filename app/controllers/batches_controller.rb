9e899451b27ca0c8292f147cde165e87493a0fec
Diego Saverys <diegosaverys@gmail.com>
2019-03-15 15:32:37 +0100

    batch creation, links, ...

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -1,2 +1,47 @@
 class BatchesController < ApplicationController
+
+  before_action :verify_role, only: [:new, :create]
+
+  def index
+    @user = current_user
+    @beer = Beer.find(params[:beer_id])
+    @batches = @beer.batches
+
+  end
+
+  def show
+    @user = current_user
+    @batch = Batch.find(params[:id])
+  end
+
+  def new
+    @batch = Batch.new
+    @beer = Beer.find(params[:beer_id])
+  end
+
+  def create
+    @batch = Batch.new(batch_params)
+    @batch.beer = Beer.find(params[:beer_id])
+    @batch.brewery = @batch.beer.brewery
+    if @batch.save!
+      redirect_to beer_batches_path(@batch.beer)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def batch_params
+    params.require(:batch).permit(:bottled_date, :description, :quantity)
+
+  end
+
+  def verify_role
+    if !current_user.admin?
+      redirect_to root_path
+    end
+  end
 end
+
+
